04e5975baf5766ff8c6c2ef0fe44d6caae115c89
LG-blip <g021306@gmail.com>
2022-02-14 18:50:21 +0530

    Namecard-Flutter
    Signed-off-by: LG-blip <g021306@gmail.com>

--- other apps/namecard.dart
+++ other apps/namecard.dart
@@ -8,6 +8,7 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.tealAccent,
         body: SafeArea(
